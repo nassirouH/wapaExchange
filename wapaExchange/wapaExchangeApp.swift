134494cf9b61c8f90719729d82fd9ce37df09fb2
hass <naswagen@gmil.com>
2026-06-18 19:26:21 +0200

    Initial Commit

---
+++ wapaExchange/wapaExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  wapaExchangeApp.swift
+//  wapaExchange
+//
+//  Created by Nassirou M Hassan on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct wapaExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
